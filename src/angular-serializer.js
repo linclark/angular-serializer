763051a307bf9f5208d04d0caa3a2e47839b9253
linclark <lin.w.clark@gmail.com>
2014-03-26 15:41:16 -0400

    Linting.

--- src/angular-serializer.js
+++ src/angular-serializer.js
@@ -3,79 +3,86 @@
  * (c) 2014 Lin Clark http://lin-clark.com
  * License: MIT
  */
+(function () {
+    'use strict';
 
-(function() {
-  angular.module('linclark.serializer', [])
-    // Concrete serializer for x-www-form-urlencoded.
-    .provider('lcFormUrlencodedSerializer', function() {
-      return {
-        $get: function () {
-          return {
-            // Serialize the data. Adapted from jQuery's param(). Currently
-            // supports a limited number of data structures, as tested in the
-            // unit tests.
-            'serialize': function(data) {
-              var r20 = /%20/g,
-                prefix,
-                s = [];
-              var add = function(key, value) {
-                  // If value is a function, invoke it and return its value
-                  value = angular.isFunction(value) ? value() : (value == null ? "" : value);
-                  s[ s.length ] = encodeURIComponent(key) + "=" + encodeURIComponent(value);
-                },
-                buildParams = function(prefix, obj, add) {
-                  // Recursively encode nested structures.
-                  if (angular.isObject(obj)) {
-                    for (name in obj) {
-                      buildParams(prefix + "[" + name + "]", obj[name], add);
+    angular.module('linclark.serializer', [])
+        // Concrete serializer for x-www-form-urlencoded.
+        .provider('lcFormUrlencodedSerializer', function () {
+            return {
+                $get: function () {
+                    return {
+                        // Serialize the data. Adapted from jQuery's param(). Currently
+                        // supports a limited number of data structures, as tested in the
+                        // unit tests.
+                        'serialize': function (data) {
+                            var r20 = /%20/g,
+                                prefix,
+                                s = [],
+                                add = function (key, value) {
+                                    // If value is a function, invoke it and return its value
+                                    value = angular.isFunction(value) ? value() : (value === null ? '' : value);
+                                    s[s.length] = encodeURIComponent(key) + '=' + encodeURIComponent(value);
+                                },
+                                buildParams = function (prefix, obj, add) {
+                                    // Recursively encode nested structures.
+                                    if (angular.isObject(obj)) {
+                                        for (var name in obj) {
+                                            if (obj.hasOwnProperty(name)) {
+                                                buildParams(prefix + '[' + name + ']', obj[name], add);
+                                            }
+                                        }
+                                    }
+                                    else {
+                                        add(prefix, obj);
+                                    }
+                                };
+
+                            for (prefix in data) {
+                                if (data.hasOwnProperty(prefix)) {
+                                    buildParams(prefix, data[prefix], add);
+                                }
+                            }
+                            return s.join('&').replace(r20, '+');
+                        }
+                    };
+                }
+            };
+        }
+    )
+    .provider('lcSerializer', function(lcFormUrlencodedSerializerProvider) {
+        return {
+            $get: function ($q) {
+                // @todo Break this out into a mediatype negotiator service.
+                var getMediatype = function (headers) {
+                    var mediatype = 'json';
+
+                    // If Content-Type is set, use that.
+                    if (angular.isDefined(headers['Content-Type']) && headers['Content-Type'] == 'application/x-www-form-urlencoded') {
+                        mediatype = 'form-urlencoded';
                     }
-                  }
-                  else {
-                    add(prefix, obj);
-                  }
+                    return mediatype;
                 };
 
-              for (prefix in data) {
-                buildParams(prefix, data[prefix], add);
-              }
-              return s.join("&").replace(r20, "+");
+                return {
+                    // Serialize the data.
+                    'request': function(config) {
+                        if (angular.isDefined(config.headers) && getMediatype(config.headers) == 'form-urlencoded') {
+                            var serializer = lcFormUrlencodedSerializerProvider.$get();
+                            config.data = serializer.serialize(config.data);
+                        }
+                        return config || $q.when(config);
+                    },
+                    // Deserialize the data.
+                    'response': function(response) {
+                        // @todo Add deserilization support.
+                        return response;
+                    }
+                };
             }
-          }
-        }
-      }
-    })
-    .provider('lcSerializer', function(lcFormUrlencodedSerializerProvider) {
-      return {
-        $get: function ($q) {
-          // @todo Break this out into a mediatype negotiator service.
-          var getMediatype = function (headers) {
-            var mediatype = "json";
-
-            // If Content-Type is set, use that.
-            if (angular.isDefined(headers["Content-Type"]) && headers["Content-Type"] == 'application/x-www-form-urlencoded') {
-              return 'form-urlencoded';
-            }
-          };
-
-          return {
-            // Serialize the data.
-            'request': function(config) {
-              if (angular.isDefined(config.headers) && getMediatype(config.headers) == 'form-urlencoded') {
-                var serializer = lcFormUrlencodedSerializerProvider.$get();
-                config.data = serializer.serialize(config.data);
-              }
-              return config || $q.when(config);
-            },
-            // Deserialize the data.
-            'response': function(response) {
-              // @todo Add deserilization support.
-              return response;
-            }
-          };
-        }
-      }
+        };
     })
     .config(['$httpProvider', function($httpProvider) {
-      $httpProvider.interceptors.push('lcSerializer');
-    }])
+        $httpProvider.interceptors.push('lcSerializer');
+    }]);
 })();
--- test/angular-serializer.spec.js
+++ test/angular-serializer.spec.js
@@ -1,51 +1,51 @@
-describe("Angular Serializer", function () {
-  beforeEach(module("linclark.serializer"));
+describe('Angular Serializer', function () {
+    beforeEach(module('linclark.serializer'));
 
-  describe("serializer", function () {
-    it("should pass Content-Type: application/json through", inject(function (lcSerializer) {
-      var config = {
-        data: {
-          "title": "bar"
-        }
-      }
+    describe('serializer', function () {
+        it('should pass Content-Type: application/json through', inject(function (lcSerializer) {
+            var config = {
+                data: {
+                    'title': 'bar'
+                }
+            };
 
-      expect(lcSerializer.request(config).data).toBe(config.data);
-    }))
-    it("should delegate Content-Type: application/x-www-form-urlencoded to the form urlencoded serializer", inject(function (lcSerializer) {
-      var config = {
-        data: {
-          "title": "bar"
-        },
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded"
-        }
-      }
+            expect(lcSerializer.request(config).data).toBe(config.data);
+        }));
+        it('should delegate Content-Type: application/x-www-form-urlencoded to the form urlencoded serializer', inject(function (lcSerializer) {
+            var config = {
+                data: {
+                    'title': 'bar'
+                },
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            };
 
-      expect(lcSerializer.request(config).data).toBe("title=bar");
-    }))
-  });
-
-  describe("form urlencoded serializer", function () {
-    var serializer;
-
-    beforeEach(inject(function($injector) {
-      serializer = $injector.get("lcFormUrlencodedSerializer");
-    }));
-
-    it("should serialize an object", function () {
-      var data = {
-        "title": "Test object"
-      };
-      expect(serializer.serialize(data)).toBe("title=Test+object");
+            expect(lcSerializer.request(config).data).toBe('title=bar');
+        }));
     });
 
-    it("should serialize a nested object", function () {
-      var data = {
-        "list": {
-          "title": "Test nested object"
-        }
-      };
-      expect(serializer.serialize(data)).toBe("list%5Btitle%5D=Test+nested+object");
+    describe('form urlencoded serializer', function () {
+        var serializer;
+
+        beforeEach(inject(function ($injector) {
+            serializer = $injector.get('lcFormUrlencodedSerializer');
+        }));
+
+        it('should serialize an object', function () {
+            var data = {
+                'title': 'Test object'
+            };
+            expect(serializer.serialize(data)).toBe('title=Test+object');
+        });
+
+        it('should serialize a nested object', function () {
+            var data = {
+                'list': {
+                    'title': 'Test nested object'
+                }
+            };
+            expect(serializer.serialize(data)).toBe('list%5Btitle%5D=Test+nested+object');
+        });
     });
-  })
-})
+});
