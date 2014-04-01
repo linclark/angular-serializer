bf7b099a917d136fcf42b7a37e102fc26594391f
linclark <lin.w.clark@gmail.com>
2014-04-01 14:00:42 -0400

    Reformat to use 2 spaces.

--- src/angular-serializer.js
+++ src/angular-serializer.js
@@ -4,85 +4,85 @@
  * License: MIT
  */
 (function () {
-    'use strict';
+  'use strict';
 
-    angular.module('linclark.serializer', [])
-        // Concrete serializer for x-www-form-urlencoded.
-        .provider('lcFormUrlencodedSerializer', function () {
-            return {
-                $get: function () {
-                    return {
-                        // Serialize the data. Adapted from jQuery's param(). Currently
-                        // supports a limited number of data structures, as tested in the
-                        // unit tests.
-                        'serialize': function (data) {
-                            var r20 = /%20/g,
-                                prefix,
-                                s = [],
-                                add = function (key, value) {
-                                    // If value is a function, invoke it and return its value
-                                    value = angular.isFunction(value) ? value() : (value === null ? '' : value);
-                                    s[s.length] = encodeURIComponent(key) + '=' + encodeURIComponent(value);
-                                },
-                                buildParams = function (prefix, obj, add) {
-                                    // Recursively encode nested structures.
-                                    if (angular.isObject(obj)) {
-                                        for (var name in obj) {
-                                            if (obj.hasOwnProperty(name)) {
-                                                buildParams(prefix + '[' + name + ']', obj[name], add);
-                                            }
-                                        }
-                                    }
-                                    else {
-                                        add(prefix, obj);
-                                    }
-                                };
+  angular.module('linclark.serializer', [])
+    // Concrete serializer for x-www-form-urlencoded.
+    .provider('lcFormUrlencodedSerializer', function () {
+      return {
+        $get: function () {
+          return {
+            // Serialize the data. Adapted from jQuery's param(). Currently
+            // supports a limited number of data structures, as tested in the
+            // unit tests.
+            'serialize': function (data) {
+              var r20 = /%20/g,
+                prefix,
+                s = [],
+                add = function (key, value) {
+                  // If value is a function, invoke it and return its value
+                  value = angular.isFunction(value) ? value() : (value === null ? '' : value);
+                  s[s.length] = encodeURIComponent(key) + '=' + encodeURIComponent(value);
+                },
+                buildParams = function (prefix, obj, add) {
+                  // Recursively encode nested structures.
+                  if (angular.isObject(obj)) {
+                    for (var name in obj) {
+                      if (obj.hasOwnProperty(name)) {
+                        buildParams(prefix + '[' + name + ']', obj[name], add);
+                      }
+                    }
+                  }
+                  else {
+                    add(prefix, obj);
+                  }
+                };
 
-                            for (prefix in data) {
-                                if (data.hasOwnProperty(prefix)) {
-                                    buildParams(prefix, data[prefix], add);
-                                }
-                            }
-                            return s.join('&').replace(r20, '+');
-                        }
-                    };
+              for (prefix in data) {
+                if (data.hasOwnProperty(prefix)) {
+                  buildParams(prefix, data[prefix], add);
                 }
-            };
-        }
-    )
-    .provider('lcSerializer', function(lcFormUrlencodedSerializerProvider) {
-        return {
-            $get: function ($q) {
-                // @todo Break this out into a mediatype negotiator service.
-                var getMediatype = function (headers) {
-                    var mediatype = 'json';
-
-                    // If Content-Type is set, use that.
-                    if (angular.isDefined(headers['Content-Type']) && headers['Content-Type'] == 'application/x-www-form-urlencoded') {
-                        mediatype = 'form-urlencoded';
-                    }
-                    return mediatype;
-                };
-
-                return {
-                    // Serialize the data.
-                    'request': function(config) {
-                        if (angular.isDefined(config.headers) && getMediatype(config.headers) == 'form-urlencoded') {
-                            var serializer = lcFormUrlencodedSerializerProvider.$get();
-                            config.data = serializer.serialize(config.data);
-                        }
-                        return config || $q.when(config);
-                    },
-                    // Deserialize the data.
-                    'response': function(response) {
-                        // @todo Add deserilization support.
-                        return response;
-                    }
-                };
+              }
+              return s.join('&').replace(r20, '+');
             }
-        };
+          };
+        }
+      };
+    }
+  )
+    .provider('lcSerializer', function (lcFormUrlencodedSerializerProvider) {
+      return {
+        $get: function ($q) {
+          // @todo Break this out into a mediatype negotiator service.
+          var getMediatype = function (headers) {
+            var mediatype = 'json';
+
+            // If Content-Type is set, use that.
+            if (angular.isDefined(headers['Content-Type']) && headers['Content-Type'] == 'application/x-www-form-urlencoded') {
+              mediatype = 'form-urlencoded';
+            }
+            return mediatype;
+          };
+
+          return {
+            // Serialize the data.
+            'request': function (config) {
+              if (angular.isDefined(config.headers) && getMediatype(config.headers) == 'form-urlencoded') {
+                var serializer = lcFormUrlencodedSerializerProvider.$get();
+                config.data = serializer.serialize(config.data);
+              }
+              return config || $q.when(config);
+            },
+            // Deserialize the data.
+            'response': function (response) {
+              // @todo Add deserilization support.
+              return response;
+            }
+          };
+        }
+      };
     })
-    .config(['$httpProvider', function($httpProvider) {
-        $httpProvider.interceptors.push('lcSerializer');
+    .config(['$httpProvider', function ($httpProvider) {
+      $httpProvider.interceptors.push('lcSerializer');
     }]);
 })();
